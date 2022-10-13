2340677bd639e79564369a236601c2e8d72c9641
MohammadRahman <45675266+MohammadRahman@users.noreply.github.com>
2022-10-13 22:18:56 +0300

    prettire run successfulley

--- next.config.js
+++ next.config.js
@@ -2,17 +2,16 @@
 const webpack = require('webpack');
 const nextConfig = {
   reactStrictMode: true,
-    images: {
-            unoptimized: true
-        },
+  images: {
+    unoptimized: true
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.plugins.push(
       new webpack.ProvidePlugin({
         $: 'jquery',
         jQuery: 'jquery',
-        'window.jQuery': 'jquery',
+        'window.jQuery': 'jquery'
         // 'images.unoptimized':true
-       
       })
     );
     return config;
